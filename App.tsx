cd2a783e6bb274b22b66933b756e1704693faa6b
Pedro de Paula <pmdpaula@gmail.com>
2023-02-18 15:52:29 -0300

    feat(navigation): tab bottom de navegação

--- App.tsx
+++ App.tsx
@@ -9,13 +9,13 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 
-import { StatusBar } from 'expo-status-bar';
-
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
-import Register from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes';
+
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +30,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -132,4 +132,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export { Register };
